e5db838d21e3556778ce1c56b74c18e67d2a81ff
hakimamarullah <hakimamarullah@gmail.com>
2025-07-16 23:18:45 +0700

    Increase driver wait time

--- src/main/java/com/starline/scrapper/ScrapperServiceApplication.java
+++ src/main/java/com/starline/scrapper/ScrapperServiceApplication.java
@@ -2,9 +2,12 @@ package com.starline.scrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class ScrapperServiceApplication {
 
 	public static void main(String[] args) {
