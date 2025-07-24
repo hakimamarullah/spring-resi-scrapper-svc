8d4a7bae1878532af41238bb7fce5d5721320801
hakimamarullah <hakimamarullah@gmail.com>
2025-07-24 08:12:42 +0700

    Remove url param from feign

--- src/main/java/com/starline/scrapper/feign/ScraperPuppeteerProxy.java
+++ src/main/java/com/starline/scrapper/feign/ScraperPuppeteerProxy.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${srv.feign.names.scraper-puppeteer:scraper-puppeteer-svc}", url = "http://localhost:3000", configuration = {FeignBasicConfig.class})
+@FeignClient(name = "${srv.feign.names.scraper-puppeteer:scraper-puppeteer-svc}", configuration = {FeignBasicConfig.class})
 public interface ScraperPuppeteerProxy {
 
     @GetMapping(value = "/track")
